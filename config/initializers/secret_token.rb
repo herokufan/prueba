b0d233fd94cefee8150dae5f2b7d26aad2e3ee12
Heroku <heroku.fan@gmail.com>
2013-08-08 23:34:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestTraining::Application.config.secret_key_base = 'a12456ceaecd484000ae513604b47936b219d41f010e19416aae96c2323e1f4e25318510be2bf74edc2103981dd8ad386218dca9e755243d576830dc8aa70b00'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestTraining::Application.config.session_store :cookie_store, key: '_test_training_session'
